de515a40ac09d8ac6639ed3cfef0c01664229bdd
EN-BAAK <swaiengineer@gmail.com>
2025-05-08 02:00:52 +0300

    add 'view dataset batches', 'convert to gray scale' method

--- helpers.py
+++ helpers.py
@@ -1,5 +1,7 @@
 from typing import Optional, Tuple, Union
 from math import floor
+import matplotlib.pyplot as plt
+import tensorflow as tf
 
 def split_data_set(
     dataset,
@@ -32,3 +34,24 @@ def split_data_set(
         return train_ds, validate_ds, test_ds
     else:
         return train_ds, test_ds
+
+
+def view_dataset_batches(dataset, ar_classes, take=1, no_imgs=12, figsize=10, rows=3, columns=4):
+    plt.figure(figsize=(figsize, figsize))
+
+    for image_batch, labels_batch in dataset.take(take):
+        for i in range(min(no_imgs, len(image_batch))):
+            plt.subplot(rows, columns, i + 1)
+            plt.imshow(image_batch[i].numpy().astype("uint8"))
+
+            label_index = labels_batch[i].numpy()
+            
+            plt.title(ar_classes[label_index])
+            plt.axis("off")
+
+    plt.tight_layout()
+    plt.show()
+
+def convert_to_gray_scale(img, label):
+    img = tf.image.rgb_to_grayscale(img)
+    return img, label
